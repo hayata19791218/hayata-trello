46cfb19a01be393c5c0f68ef311d3860e9e41cc9
hayata19791218 <hayatakyohei@kzh.biglobe.ne.jp>
2022-03-03 12:55:04 +0900

    3/3のデータ

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Board/>
+    <ListAdd/>
+  </div>
+</template>
+
+<script>
+import Board from './components/Board.vue'
+import ListAdd from  './components/ListAdd.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Board,
+    ListAdd
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ListAdd.vue
@@ -0,0 +1,35 @@
+<template>
+    <form 
+        @submit.prevent="addList" 
+        class="addlist"
+    >
+        <input 
+            type="text" 
+            class="text-input"
+            v-modle="title"
+            placeholder="Add new list"
+        >
+        <button 
+            class="add-button"
+            type="submit"
+        >
+            Add
+        </button>
+    </form>
+</template>
+
+<script>
+export default{
+    data:function(){
+        return{
+            title:''
+        }
+    },
+    methods:{
+        addList:function(){
+            this.$store.dispatch('addlist',{title:this.title})
+            this.title=''
+        }
+    }
+}
+</script>
